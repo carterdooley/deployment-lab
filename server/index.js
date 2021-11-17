d32553fdffa12e5de70b1e6afa606b446f732e10
carter dooley <carter.j.dooley43@gmail.com>
2021-11-17 14:36:37 -0600

    Update Css

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/main.js'))
+//   })
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../style.css'))
+  })
 
 const port = process.env.PORT || 4005
 
