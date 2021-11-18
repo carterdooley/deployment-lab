db68f3ca17bf76f3610f5931297539159cecf8dc
carter dooley <carter.j.dooley43@gmail.com>
2021-11-18 15:02:32 -0600

    RollBar Update

--- server/index.js
+++ server/index.js
@@ -6,17 +6,19 @@ const app = express()
 
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: '881dcde6339947578e6d3c60b181042b',
+  accessToken: '1689ca6392ec439caf91d04f46a7b9ce',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
 
-app.get('/get', (req, res) => {try {
-  nonExistentFunction();
-} catch (error) {
-  console.error(error);
-}
-})
+rollbar.log('Working')
+
+// app.get('/get', (req, res) => {try {
+//   nonExistentFunction();
+// } catch (error) {
+//   rollbar.error(error);
+// }
+// })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
