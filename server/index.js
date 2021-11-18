d0f15b47ee0bd516310d97ae6e18efd08d4fa936
carter dooley <carter.j.dooley43@gmail.com>
2021-11-18 14:43:48 -0600

    RollBar Update

--- server/index.js
+++ server/index.js
@@ -4,19 +4,17 @@ const path = require('path')
 
 const app = express()
 
-// var Rollbar = require('rollbar')
-// var rollbar = new Rollbar({
-//   accessToken: 'ac5546d947ba4ebab06a8e2b4d254937',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-// })
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '881dcde6339947578e6d3c60b181042b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
-// rollbar.log('Hello world!')
-
-// server.get('/get', (req, res) => {
-//   res.send('Welcome to API')
-//   rollbar.info('User interacted')
-// })
+.get('/get', (req, res) => {
+  res.send('Welcome to API')
+  rollbar.info('User interacted')
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
@@ -29,7 +27,6 @@ app.get('/style.css', (req, res) => {
 
 const port = process.env.PORT || 4005
 
-const PORT = process.env.Port || 7000
-server.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+app.listen(port, () => {
+    console.log(`App running on ${port}`)
 })
