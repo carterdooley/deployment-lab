cfcc65af513f6233c68aaec9e5788e31f09ce532
carter dooley <carter.j.dooley43@gmail.com>
2021-11-18 16:47:31 -0600

    Functionality Start

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const uploadFav = () => {
     app.post('https://deployment-lab-carter.herokuapp.com/posthalogame')
 }
 
-
-
+const haloText = () => {
+    let haloName = document.getElementById('sub').textContent
+    
+}
+//see movie list to finish
 
 const subFav = () => {
     const button = document.getElementById('subBut')
-    button.addEventListener('click', uploadFav )
+    button.addEventListener('click', haloText )
 }
